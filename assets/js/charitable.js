e796ebf2c1d884eb0f7f0abeb80b211a70fab176
Eric Daams <eric@164a.com>
2019-06-28 10:29:11 +0000

    Added method to check whether a donation is recurring.

--- assets/js/charitable.js
+++ assets/js/charitable.js
@@ -266,17 +266,7 @@ CHARITABLE = window.CHARITABLE || {};
                     withCredentials: true
                 },
                 success: function (response) {
-                    // var errors;
-
                     $body.trigger( 'charitable:form:processed', [ response, helper ] );
-
-                    // if ( response.success ) {
-                    //     maybe_process( helper, function() {
-                    //         window.location.href = response.redirect_to;
-                    //     } );
-                    // } else {
-
-                    // }
 
                     if ( response.success ) {
                         maybe_process( helper, function() {
