fde75e664bf74fd990c17c012d6cc7de79238fb3
Eric Daams <eric@164a.com>
2020-09-16 16:31:46 +0930

    Minor formatting and doc commenting

--- includes/admin/views/settings/multi-checkbox.php
+++ includes/admin/views/settings/multi-checkbox.php
@@ -23,11 +23,9 @@ if ( ! is_array( $value ) ) {
 $setting_attributes = '';
 
 if ( array_key_exists( 'attrs', $view_args ) ) {
-
 	$setting_attributes_keys = array( 'data-trigger-key', 'data-trigger-change-type', 'data-trigger-value' );
 
 	foreach ( $setting_attributes_keys as $key ) {
-
 		if ( ! array_key_exists( $key, $view_args['attrs'] ) ) {
 			continue;
 		}
@@ -35,29 +33,24 @@ if ( array_key_exists( 'attrs', $view_args ) ) {
 		$setting_attributes .= sprintf( ' %s="%s"', $key, esc_attr( $view_args['attrs'][ $key ] ) );
 
 		unset( $view_args['attrs'][ $key ] );
-
 	}
 }
 
 ?>
 <ul class="charitable-checkbox-list <?php echo esc_attr( $view_args['classes'] ); ?>" <?php echo $setting_attributes; ?>>
-
 	<?php foreach ( $view_args['options'] as $option => $label ) : ?>
-
 		<li><input type="checkbox"
 				id="<?php printf( 'charitable_settings_%s_%s', implode( '_', $view_args['key'] ), $option ); ?>"
 				name="<?php printf( 'charitable_settings[%s][]', $view_args['name'] ); ?>"
 				value="<?php echo esc_attr( $option ); ?>"
-				<?php checked( in_array( $option, $value ) ); ?>
+				<?php checked( in_array( $option, $value, true ) ); ?>
 				<?php echo charitable_get_arbitrary_attributes( $view_args ); ?>
 			/>
 			<?php echo $label; ?>
 		</li>
-
 	<?php endforeach ?>
-
 </ul>
 <?php if ( isset( $view_args['help'] ) ) : ?>
 	<div class="charitable-help"><?php echo $view_args['help']; ?></div>
-<?php
+	<?php
 endif;
