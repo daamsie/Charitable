cd67a3abfcad76360dd176eac331b73476efb0bd
Ben Jackson <z5215452@ad.unsw.edu.au>
2020-12-16 16:16:11 +0930

    Now rounds to 2 decimal places, cosmetic and equality issues fixed

--- includes/shortcodes/class-charitable-stat-shortcode.php
+++ includes/shortcodes/class-charitable-stat-shortcode.php
@@ -90,8 +90,8 @@ if ( ! class_exists( 'Charitable_Stat_Shortcode' ) ) :
 					$total   = charitable_sanitize_amount( $total, true );
 					$percent = ( $total / $goal ) * 100;
 
-					if ($this->type =='percentage' ) {
-						return (string) $percent . "%";
+					if ( $this->type === 'percentage' ) {
+						return (string) number_format( $percent, 2 ) . "%";
 					}
 
 					return '<div class="campaign-progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="' . $percent . '"><span class="bar" style="width:' . $percent . '%;"></span></div>';
